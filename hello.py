e232825d3d0ce3030f08a54c14296b9f188e6fc0
unknown <agradhika@gmail.com>
2020-07-29 23:12:12 -0700

    hello.py and moreif.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+
+print("Hello!")
+x = "Hello again!!"
+
+print(x)
+
+x='Single quotes Hello :)'
+
+print(x)
---
+++ moreIf.py
@@ -0,0 +1,19 @@
+uval = input("Enter your value: ")
+wval = int(uval)
+
+if wval == 1:
+    print("It is Monday!!")
+elif wval == 2:
+    print("It is Tuesday!!")
+elif wval ==3 :
+    print("It is Wednesday!!")
+elif wval == 4:
+    print("It is Thursday!!")
+elif wval == 5:
+    print("It is Friday!!")
+elif wval == 6:
+    print("It is Saturday!!")
+elif wval == 7:
+    print("It is Sunday!!")
+else:
+    print("It is invalid weekday")
